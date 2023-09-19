3d064038890df1837c755163663fe16bbe44d610
vikas8706 <145487396+vikas8706@users.noreply.github.com>
2023-09-19 18:02:50 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 8826
+let accountName = "vikas"
+var accountEmail = "vikashsharma8706@gmail.com"
+accountCity = "Noida"
+
+console.table([accountId, accountName, accountEmail, accountCity])

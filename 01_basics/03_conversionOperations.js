8c382855b0eefe532d18ca5281dbf18d8d44058e
vikas8706 <145487396+vikas8706@users.noreply.github.com>
2023-09-20 11:35:33 +0000

    conversation and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,44 @@
+let score = undefined
+
+// console.log(typeof score);  //typeof used to find the datatype of the variable 
+
+let valueInNumber = Number(score) // Number is used to convert the string value into number data type
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+//Notes : "33" => 33 converted to number
+// "33abc" => NaN   (Not A Number)
+// true => 1 , false => 0 
+
+
+// we can convert strings to numbers, numbers to strings to booleans by following the same method like Number(variable), String(variable)
+
+
+
+
+/***************Operations **************************/
+
+let value = 3
+let negVal = -value  //this will make the value in negative like -3
+
+/*some common operations*/
+// console.log(2+2);  addition
+// console.log(2-2); subtraction
+// console.log(2*2); multiple
+// console.log(2**4); power 2 to the power 4
+// console.log(2%2); reminder calculate
+// console.log(2/2); divide
+
+let str1 = 2
+let str2 = 3
+let str3 = str1 + str2
+
+console.log(str3);
+
+
+
+
+
+
